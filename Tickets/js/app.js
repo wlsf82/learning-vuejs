f81a507dd9f15a4f2e05acb3cdd9cd5d3e36a38d
Walmyr Filho <limaesilva@qelp.com>
2019-07-12 23:40:37 +0200

    Replace some static with dynamic data

--- Tickets/js/app.js
+++ Tickets/js/app.js
@@ -1,3 +1,13 @@
 const app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    ticketQuantity: 1,
+    ticketType: 'general',
+    referrals: [],
+    specialRequests: '',
+    purchaseAgreementSigned: false
+  }
 });
