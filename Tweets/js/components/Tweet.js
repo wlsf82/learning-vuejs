846b847628da887483fa43ebae83640a0967488d
Walmyr Filho <limaesilva@qelp.com>
2019-07-18 22:06:27 +0200

    Improves html and component readability

--- Tweets/js/components/Tweet.js
+++ Tweets/js/components/Tweet.js
@@ -1,4 +1,7 @@
 const Tweet = Vue.component('tweet', {
   props: ['message', 'author'],
-  template: '<div class="tweet"><h3>{{ author }}</h3><p>{{ message }}</p></div>'
+  template: `<div class="tweet">
+               <h3>{{ author }}</h3>
+               <p>{{ message }}</p>
+             </div>`
  });
