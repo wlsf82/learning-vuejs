ca30647c8d9c8428bfc4168145404f4fc5b01b6b
Walmyr Filho <limaesilva@qelp.com>
2019-07-30 22:30:30 +0200

    Add missing semi-colons on tests

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -2,7 +2,7 @@ describe("Tickets", () => {
   beforeEach(() => cy.visit("https://bit.ly/2XSuwCW"));
 
   it("has 'TICKETBOX' header's heading", () => {
-    cy.get("header h1").should("contain", "TICKETBOX")
+    cy.get("header h1").should("contain", "TICKETBOX");
   });
 
   it("fills all the text input fields", () => {
--- cypress/integration/tweets.spec.js
+++ cypress/integration/tweets.spec.js
@@ -2,10 +2,10 @@ describe("Tweets", () => {
   beforeEach(() => cy.visit("https://bit.ly/2Git95m"));
 
   it("has 'THE VUE VIEW' main heading", () => {
-    cy.get("h1").should("contain", "THE VUE VIEW")
+    cy.get("h1").should("contain", "THE VUE VIEW");
   });
 
   it("has a visible avatar", () => {
-    cy.get(".image-container img").should("be.visible")
+    cy.get(".image-container img").should("be.visible");
   });
 });
