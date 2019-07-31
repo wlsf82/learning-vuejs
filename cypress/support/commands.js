7386131123b2e8284a5fb68ad327dc54e94983fe
Walmyr Filho <limaesilva@qelp.com>
2019-08-01 00:29:15 +0200

    Create tweet custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("tweet", (text) => {
+  cy.get(".new-tweet input").type(text);
+  cy.contains("Add Tweet").click();
+});
